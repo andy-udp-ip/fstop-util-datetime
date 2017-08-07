72d3cbbaabcd0399ca060866caefb2efc85dc5af
andy <andy.udp.ip@gmail.com>
2017-08-08 01:08:20 +0800

    Add date time functions

--- src/main/java/tw/com/fstop/util/DateTimeUtil.java
+++ src/main/java/tw/com/fstop/util/DateTimeUtil.java
@@ -13,13 +13,16 @@
 
 package tw.com.fstop.util;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 /**
+ * <pre>
  * Class for process Date and Time, based on epoch calculation.
- * In case to avoid class naming collision with other third party class, this class is rename from DateTime to DateTimeUtil. 
+ * To avoid class naming collision with other third party class, this class is renamed from DateTime to DateTimeUtil. 
  *
  * 23:59 is the last minute of the day and 00:00 is the first minute of the next day. 
  * Thus, 23:59 on Dec 31, 1999 < 00:00 on Jan 1, 2000 < 00:01 on Jan 1, 2000.
@@ -50,6 +53,7 @@ import java.util.TimeZone;
  * 
  * ISO8601 的定義中，19 世紀是指 1900 至 1999 年（包含該年）
  * 格里高利曆的 19 世紀是指 1801 年至 1900 年（包含該年）
+ * </pre>
  * 
  * @since 1.0
  */
@@ -89,9 +93,11 @@ public class DateTimeUtil
         //milliseconds -> seconds -> hour
         return getCurrentTimeZoneOffset() / 1000 / 3600;
     }
-    
+        
     /**
-     * Convert current time to EPOCH without milliseconds.
+     * Get current EPOCH milliseconds.
+     * Here epoch is always UTC based, no need to convert.
+     * 
      * @return      epoch millisecond
      */
     public static long getCurrentEpoch()
@@ -100,16 +106,90 @@ public class DateTimeUtil
     }
     
     /**
-     * Convert current time to EPOCH without seconds.
-     * Epoch is always UTC based, no need to convert.
+     * Get current EPOCH seconds.
+     * Here epoch is always UTC based, no need to convert.
      * 
-     * @return      epoch second
+     * @return      current epoch second
      */
     public static long getCurrentEpochSecond()
     {
         return (getCurrentEpoch()/1000);
     }
-        
+
+    /**
+     * Get current EPOCH seconds.
+     * Value returned by this function should be the same with getCurrentEpochSecond().
+     * 
+     * @return      current epoch second
+     */
+    public static long getEpochSecond()
+    {
+        TimeZone timeZone = TimeZone.getTimeZone(TIMEZONE_ID_UTC);
+        Calendar calendar = Calendar.getInstance(timeZone);
+        long secondsSinceEpoch = calendar.getTimeInMillis()/1000L;
+        return secondsSinceEpoch;
+    }          
+    
+    /**
+     * Get specified datetime string.
+     * 
+     * @param dtFormat      datetime format
+     * @return              formatted datetime string
+     */
+    public static String getCurrentDateString(String dtFormat)
+    {
+        DateFormat format = new SimpleDateFormat(dtFormat);
+        Calendar calendar = Calendar.getInstance();       
+        String formatted = format.format(calendar.getTime());        
+        return formatted;
+    }
+    
+    
+    /**
+     * Get current year.
+     * @return      year
+     */
+    public static int getCurrentYear()
+    {
+        Calendar  calendar = null;
+        calendar = Calendar.getInstance();
+        return calendar.get(Calendar.YEAR);     
+    }
+    
+    /**
+     * Get current month.
+     * @return      month
+     */
+    public static int getCurrentMonth()
+    {
+        Calendar  calendar = null;
+        calendar = Calendar.getInstance();
+        return 1 + calendar.get(Calendar.MONTH);
+    }
+
+    /**
+     * Get current day.
+     * @return      day
+     */
+    public static int getCurrentDay()
+    {
+        Calendar  calendar = null;
+        calendar = Calendar.getInstance();
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    /**
+     * Get local day.
+     * @return      day
+     */
+    public static int getLocalDay(String timezoneID)
+    {
+        TimeZone timeZone = TimeZone.getTimeZone(timezoneID);
+        Calendar  calendar = null;
+        calendar = Calendar.getInstance(timeZone);
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+    
     /**
      * Get current system timezone offset value.
      * @param tzID  timezone id
@@ -164,7 +244,7 @@ public class DateTimeUtil
     public static int getLocalDaylightSavingOffset(String timezoneID)
     {
         TimeZone timeZone = TimeZone.getTimeZone(timezoneID);
-        Calendar calendar = Calendar.getInstance();
+        Calendar calendar = Calendar.getInstance(timeZone);
         int dlsOffset = calendar.get(Calendar.DST_OFFSET);
         return dlsOffset;
     }
@@ -185,14 +265,14 @@ public class DateTimeUtil
      * Input year, month, day, hour, minute, second and return UTC epoch seconds.
      * 
      * @param year
-     * @param month     Month value form 1 to 12
+     * @param month     month value from 1 to 12
      * @param day
      * @param hour
      * @param minute
      * @param second
      * @return          epoch seconds
      */
-    public static long getUtcEpochSecond(int year, int month, int day, int hour, int minute, int second)
+    public static long getEpochSecond(int year, int month, int day, int hour, int minute, int second)
     {
         TimeZone timeZone = TimeZone.getTimeZone(TIMEZONE_ID_UTC);
         Calendar calendar = Calendar.getInstance(timeZone);
@@ -203,4 +283,318 @@ public class DateTimeUtil
         return secondsSinceEpoch;
     }          
 
+    /**
+     * Input datetime string and return UTC epoch seconds.
+     * 
+     * @param yyyyMMddhhmiss
+     * @return          epoch seconds
+     */
+    public static long getEpochSecond(String yyyyMMddhhmiss)
+    {
+        long ret = 0;
+        int yyyy = Integer.parseInt(yyyyMMddhhmiss.substring(0, 4));
+        int mm = Integer.parseInt(yyyyMMddhhmiss.substring(4, 6));
+        int dd = Integer.parseInt(yyyyMMddhhmiss.substring(6, 8));
+        int hh = Integer.parseInt(yyyyMMddhhmiss.substring(8, 10));
+        int mi = Integer.parseInt(yyyyMMddhhmiss.substring(10, 12));
+        int ss = Integer.parseInt(yyyyMMddhhmiss.substring(12, 14));
+        ret = getEpochSecond(yyyy, mm, dd, hh, mi, ss);       
+        return ret;
+    }
+    
+    /**
+     * Normalize UTC epoch second by input value.
+     * For example : 
+     *   Input a normalize unit 5 then this function will return epoch second which can be divide by 5. 
+     *   And returned value is less then current epoch second.
+     * 
+     * @param unit         normalize unit
+     * @return             normalized epoch second
+     */
+    public static long getNormalizedEpochSecond(int unit)
+    {
+        long second = getEpochSecond();
+        long remider = second % unit;
+        second = second - remider;
+        return second;
+    }
+
+    /**
+     * Convert epoch time to string.
+     * 
+     * @param epoch                 epoch in millisecond
+     * @param dtFormat              datetime format
+     * @param dateTimeZone          time zone of input epoch
+     * @param formateTimeZone       time zone of result datetime string, if null then use current timezone to format
+     * @return                      formatted datetime string
+     */
+    static String epochToDateString(long epoch, String dtFormat, String dtTimeZoneID, String fmtTimeZoneID)
+    {
+        TimeZone dtTimeZone = TimeZone.getTimeZone(dtTimeZoneID);
+        DateFormat format = new SimpleDateFormat(dtFormat);
+
+        if (fmtTimeZoneID != null && !fmtTimeZoneID.isEmpty())
+        {
+            TimeZone fmtTimeZone = TimeZone.getTimeZone(fmtTimeZoneID);
+            format.setTimeZone(fmtTimeZone);            
+        }
+        
+        Calendar calendar = Calendar.getInstance(dtTimeZone);
+        calendar.setTimeInMillis(epoch);        
+        String formatted = format.format(calendar.getTime());        
+        return formatted;
+    }
+
+    
+    /**
+     * Convert epoch to specified datetime string.
+     * 
+     * @param epoch             epoch time
+     * @param dtFormat          datetime format
+     * @return                  formatted datetime string
+     */
+    public static String epochToDateString(long epoch, String dtFormat)
+    {
+        return epochToDateString(epoch, dtFormat, TIMEZONE_ID_UTC, TIMEZONE_ID_UTC);
+    }
+    
+    /**
+     * Convert epoch second to specified datetime string.
+     * 
+     * @param epochSecond       epoch second
+     * @param dtFormat          datetime format 
+     * @return                  formatted datetime string
+     */
+    public static String epochSecondToDateString(long epochSecond, String dtFormat)
+    {
+        long epoch = epochSecond * 1000L;
+        return epochToDateString(epoch, dtFormat);
+    }
+
+    /**
+     * Convert utc epoch to current datetime string by specified format.
+     * 
+     * @param epoch
+     * @param dtFormat      datetime format
+     * @return              formatted datetime string
+     */
+    public static String epochToCurrentDateString(long epoch, String dtFormat)
+    {
+        return epochToDateString(epoch, dtFormat, TIMEZONE_ID_UTC, null);
+    }
+
+    /**
+     * Convert utc epoch second to current datetime string by specified format.
+     * 
+     * @param epochSecond       epoch second
+     * @param dtFormat          datetime format
+     * @return                  formatted datetime string
+     */
+    public static String epochSecondToCurrentDateString(long epochSecond, String dtFormat)
+    {
+        long epoch = epochSecond * 1000L;
+        return epochToCurrentDateString(epoch, dtFormat);
+    }
+    
+    /**
+     * Convert utc epoch to local datetime string by specified format.
+     * 
+     * @param epoch             epoch in millisecond
+     * @param dtFormat          datetime format
+     * @param timezoneID        timezone id
+     * @return                  formatted datetime string
+     */
+    public static String epochToLocalDateString(long epoch, String dtFormat, String timezoneID)
+    {
+        return epochToDateString(epoch, dtFormat, timezoneID, timezoneID);
+    }
+
+    /**
+     * Convert utc epoch to local datetime string by specified format.
+     * 
+     * @param epochSecond           epoch in second
+     * @param dtFormat              datetime format
+     * @param timezoneID            timezone id
+     * @return                      formatted datetime string
+     */
+    public static String epochSecondToLocalDateString(long epochSecond, String dtFormat, String timezoneID)
+    {
+        long epoch = epochSecond * 1000L;
+        return epochToDateString(epoch, dtFormat, timezoneID, timezoneID);
+    }
+    
+    /**
+     * Convert date string to epoch.
+     * 
+     * @param date                  date string to convert
+     * @param dtFormat              format of date string
+     * @param dtTimeZoneID          timezone of date
+     * @param fmtTimeZoneID         timezone of date format 
+     * @return                      epoch millisecond
+     * @throws ParseException
+     */
+    public static long dateStringToEpoch(String date, String dtFormat, String dtTimeZoneID, String fmtTimeZoneID) throws ParseException
+    {
+        TimeZone dtTimeZone = TimeZone.getTimeZone(dtTimeZoneID);
+        Calendar calendar = Calendar.getInstance(dtTimeZone);
+        
+        SimpleDateFormat sdf = new SimpleDateFormat(dtFormat);
+        
+        if (fmtTimeZoneID != null && !fmtTimeZoneID.isEmpty())
+        {
+            TimeZone fmtTimeZone = TimeZone.getTimeZone(fmtTimeZoneID);        
+            sdf.setTimeZone(fmtTimeZone);                    
+        }
+        
+        calendar.setTime(sdf.parse(date));
+        return calendar.getTimeInMillis();
+    }
+    
+    /**
+     * Convert date string to epoch based on utc time.
+     * 
+     * @param date              date string to convert
+     * @param dtFormat          format of date string
+     * @param fmtTimeZoneID     timezone of date format
+     * @return                  epoch millisecond
+     * @throws ParseException
+     */
+    public static long dateStringToEpoch(String date, String dtFormat, String fmtTimeZoneID) throws ParseException
+    {
+        return dateStringToEpoch(date, dtFormat, TIMEZONE_ID_UTC, fmtTimeZoneID);
+    }
+    
+    /**
+     * Convert date string to epoch based on utc time.
+     * 
+     * @param date                  date string to convert
+     * @param dtFormat              format of date string
+     * @param fmtTimeZoneID         timezone of date format
+     * @return                      epoch second
+     * @throws ParseException
+     */
+    public static long dateStringToEpochSecond(String date, String dtFormat, String fmtTimeZoneID) throws ParseException
+    {
+        long epoch = dateStringToEpoch(date, dtFormat, fmtTimeZoneID);
+        return epoch / 1000L;
+    }
+    
+    /**
+     * Calculate days between two dates.
+     * Input sequence of the two dates is not important. 
+     * 
+     * @param day1
+     * @param day2
+     * @return          days between two dates
+     */
+    public static int daysBetween(Calendar day1, Calendar day2)
+    {
+        Calendar dayOne = (Calendar) day1.clone(), dayTwo = (Calendar) day2.clone();
+
+        if (dayOne.get(Calendar.YEAR) == dayTwo.get(Calendar.YEAR))
+        {
+            return Math.abs(dayOne.get(Calendar.DAY_OF_YEAR) - dayTwo.get(Calendar.DAY_OF_YEAR));
+        }
+        else
+        {
+            // if day2 > day1 then swap them
+            if (dayTwo.get(Calendar.YEAR) > dayOne.get(Calendar.YEAR))
+            {
+                Calendar temp = dayOne;
+                dayOne = dayTwo;
+                dayTwo = temp;
+            }
+            int extraDays = 0;
+
+            int dayOneOriginalYearDays = dayOne.get(Calendar.DAY_OF_YEAR);
+
+            while (dayOne.get(Calendar.YEAR) > dayTwo.get(Calendar.YEAR))
+            {
+                dayOne.add(Calendar.YEAR, -1);
+                // getActualMaximum() important for leap years
+                extraDays += dayOne.getActualMaximum(Calendar.DAY_OF_YEAR);
+            }
+
+            return extraDays - dayTwo.get(Calendar.DAY_OF_YEAR) + dayOneOriginalYearDays;
+        }
+    }
+    
+    /**
+     * Calculate days between two dates.
+     * 
+     * @param day1
+     * @param day2
+     * @param dtFormat          datetime format
+     * @param fmtTimeZoneID1    timezone id of day1
+     * @param fmtTimeZoneID2    timezone id of day2
+     * @return                  days between two dates
+     * @throws ParseException
+     */
+    static int daysBetween(String day1, String day2, String dtFormat, String fmtTimeZoneID1, String fmtTimeZoneID2) throws ParseException
+    {
+        long epoch1 = dateStringToEpoch(day1, dtFormat, fmtTimeZoneID1);
+        long epoch2 = dateStringToEpoch(day2, dtFormat, fmtTimeZoneID2);
+        TimeZone dtTimeZone = TimeZone.getTimeZone(TIMEZONE_ID_UTC);
+        Calendar calendar1 = Calendar.getInstance(dtTimeZone);
+        calendar1.setTimeInMillis(epoch1);        
+        Calendar calendar2 = Calendar.getInstance(dtTimeZone);
+        calendar2.setTimeInMillis(epoch2);   
+        return daysBetween(calendar1, calendar2);        
+    }
+
+    /**
+     * Calculate days between two dates of current timezone.
+     * 
+     * @param day1
+     * @param day2
+     * @param dtFormat          datetime format
+     * @return                  days between two dates
+     * @throws ParseException
+     */
+    public static int daysBetween(String day1, String day2, String dtFormat) throws ParseException
+    {
+        String timezoneID = getCurrentTimeZoneID();
+        long epoch1 = dateStringToEpoch(day1, dtFormat, timezoneID);
+        long epoch2 = dateStringToEpoch(day2, dtFormat, timezoneID);
+        TimeZone dtTimeZone = TimeZone.getTimeZone(TIMEZONE_ID_UTC);
+        Calendar calendar1 = Calendar.getInstance(dtTimeZone);
+        calendar1.setTimeInMillis(epoch1);        
+        Calendar calendar2 = Calendar.getInstance(dtTimeZone);
+        calendar2.setTimeInMillis(epoch2);   
+        return daysBetween(calendar1, calendar2);        
+    }
+
+    /**
+     * Calculate days between two epoch dates.
+     * 
+     * @param epoch1
+     * @param epoch2
+     * @return                  days between two dates
+     * @throws ParseException
+     */
+    public static int daysBetween(long epoch1, long epoch2) throws ParseException
+    {
+        TimeZone dtTimeZone = TimeZone.getTimeZone(TIMEZONE_ID_UTC);
+        Calendar calendar1 = Calendar.getInstance(dtTimeZone);
+        calendar1.setTimeInMillis(epoch1);        
+        Calendar calendar2 = Calendar.getInstance(dtTimeZone);
+        calendar2.setTimeInMillis(epoch2);   
+        return daysBetween(calendar1, calendar2);        
+    }
+    
+    /**
+     * Calculate days between two epoch dates.
+     * 
+     * @param epochSecond1      epoch seconds
+     * @param epochSecond2      epoch seconds
+     * @return                  days between two dates
+     * @throws ParseException
+     */
+    public static int daysBetweenEpochSecond(long epochSecond1, long epochSecond2) throws ParseException
+    {
+        long epoch1 = epochSecond1 * 1000L;
+        long epoch2 = epochSecond2 * 1000L;
+        return daysBetween(epoch1, epoch2);        
+    }
+    
 }
--- src/test/java/tw/com/fstop/util/DateTimeTest.java
+++ src/test/java/tw/com/fstop/util/DateTimeTest.java
@@ -16,6 +16,8 @@ package tw.com.fstop.util;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.text.ParseException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -35,7 +37,7 @@ public class DateTimeTest
     }
 
     @Test
-    public void testGetEpoch()
+    public void testGetEpoch() throws ParseException
     {
         String tzID = DateTimeUtil.getCurrentTimeZoneID();
         assertNotNull(tzID);
@@ -72,17 +74,141 @@ public class DateTimeTest
         System.out.println(dlsOffset);
         assertNotNull(dlsOffset);
         
-        long t = DateTimeUtil.getUtcEpochSecond(1970, 1, 1, 0, 0, 0);
+        long t = DateTimeUtil.getEpochSecond(1970, 1, 1, 0, 0, 0);
         assertThat(t).isZero();
 
+        t = DateTimeUtil.getEpochSecond("19700101000000");
+        assertThat(t).isZero();
+        
         t = DateTimeUtil.getCurrentEpoch();
         System.out.println(t);
         assertNotNull(t);
         
-        t = DateTimeUtil.getCurrentEpochSecond();
-        System.out.println(t);
-        assertNotNull(t);
+        long t1 = DateTimeUtil.getCurrentEpochSecond();
+        assertNotNull(t1);
         
+        long t2 = DateTimeUtil.getEpochSecond();
+        assertNotNull(t2);
+        
+        assertThat(t1).isEqualTo(t2);
+        
+        t = DateTimeUtil.getNormalizedEpochSecond(3);
+        System.out.println(t);
+        assertThat(t % 3).isZero();
+        
+        String fmt = DateTimeUtil.epochSecondToDateString(0, "yyyy-MM-dd HH:mm:ss");
+        System.out.println(fmt);
+        assertThat(fmt).isEqualTo("1970-01-01 00:00:00");
+
+        fmt = DateTimeUtil.epochSecondToCurrentDateString(0, "yyyy-MM-dd HH:mm:ss");
+        System.out.println(fmt);
+        assertThat(fmt).isEqualTo("1970-01-01 08:00:00");
+        
+        fmt = DateTimeUtil.getCurrentDateString("yyyy-MM-dd HH:mm:ss");
+        System.out.println(fmt);
+        assertNotNull(fmt);
+        
+        int y = DateTimeUtil.getCurrentYear();
+        System.out.println(y);
+        assertNotNull(y);
+        
+        int m = DateTimeUtil.getCurrentMonth();
+        System.out.println(m);
+        assertNotNull(m);
+
+        int d = DateTimeUtil.getCurrentDay();
+        System.out.println(d);
+        assertNotNull(d);
+        
+        // Pacific/Tarawa +12
+        int d1 = DateTimeUtil.getLocalDay("Pacific/Tarawa");
+        System.out.println(d1);
+        assertNotNull(d1);
+        
+        fmt = DateTimeUtil.epochToLocalDateString(1502114440291L, "yyyy-MM-dd HH:mm:ss", "Pacific/Tarawa");
+        System.out.println(fmt);
+        assertNotNull(fmt);
+
+        fmt = DateTimeUtil.epochToLocalDateString(1502114440291L, "yyyy-MM-dd HH:mm:ss", "Asia/Taipei");
+        System.out.println(fmt);
+        assertNotNull(fmt);
+
+        fmt = DateTimeUtil.epochToLocalDateString(1502114440291L, "yyyy-MM-dd HH:mm:ss", "UTC");
+        System.out.println(fmt);
+        assertNotNull(fmt);
+
+        fmt = DateTimeUtil.epochSecondToLocalDateString(1502115795L, "yyyy-MM-dd HH:mm:ss", "Pacific/Tarawa");
+        System.out.println(fmt);
+        assertNotNull(fmt);
+
+        fmt = DateTimeUtil.epochSecondToLocalDateString(1502115795L, "yyyy-MM-dd HH:mm:ss", "Asia/Taipei");
+        System.out.println(fmt);
+        assertNotNull(fmt);
+
+        fmt = DateTimeUtil.epochSecondToLocalDateString(1502115795L, "yyyy-MM-dd HH:mm:ss", "UTC");
+        System.out.println(fmt);
+        assertNotNull(fmt);
+        
+        t = DateTimeUtil.dateStringToEpoch("2017-08-07 22:00:40", "yyyy-MM-dd HH:mm:ss", "UTC", "Asia/Taipei");
+        System.out.println(t);
+        assertThat(t).isEqualTo(1502114440000L);
+        
+        t = DateTimeUtil.dateStringToEpoch("2017-08-07 22:00:40", "yyyy-MM-dd HH:mm:ss", "Asia/Taipei", "UTC");
+        System.out.println(t);
+        assertThat(t).isEqualTo(1502143240000L);        
+
+        t = DateTimeUtil.dateStringToEpoch("2017-08-07 22:00:40", "yyyy-MM-dd HH:mm:ss", "Asia/Taipei", "Asia/Taipei");
+        System.out.println(t);
+        assertThat(t).isEqualTo(1502114440000L);
+
+        t = DateTimeUtil.dateStringToEpoch("2017-08-07 22:00:40", "yyyy-MM-dd HH:mm:ss", "UTC", "UTC");
+        System.out.println(t);
+        assertThat(t).isEqualTo(1502143240000L);        
+        
+        t = DateTimeUtil.dateStringToEpoch("2017-08-07 22:00:40", "yyyy-MM-dd HH:mm:ss", "Asia/Taipei");
+        System.out.println(t);
+        assertThat(t).isEqualTo(1502114440000L);
+
+        t = DateTimeUtil.dateStringToEpoch("20170807", "yyyyMMdd", "Asia/Taipei");
+        System.out.println(t);
+        assertThat(t).isEqualTo(1502035200000L);
+        
+        t = DateTimeUtil.dateStringToEpoch("1970-01-01 08:00:00", "yyyy-MM-dd HH:mm:ss", "Asia/Taipei");
+        System.out.println(t);
+        assertThat(t).isEqualTo(0L); 
+
+        t = DateTimeUtil.dateStringToEpochSecond("2017-08-07 22:00:40", "yyyy-MM-dd HH:mm:ss", "Asia/Taipei");
+        System.out.println(t);
+        assertThat(t).isEqualTo(1502114440L);
+
+        t = DateTimeUtil.dateStringToEpochSecond("20170807", "yyyyMMdd", "Asia/Taipei");
+        System.out.println(t);
+        assertThat(t).isEqualTo(1502035200L);
+        
+        //2016 is leap year
+        d = DateTimeUtil.daysBetween("20160228", "20160301", "yyyyMMdd", "Asia/Taipei", "Asia/Taipei");
+        System.out.println(d);
+        assertThat(d).isEqualTo(2);
+
+        d = DateTimeUtil.daysBetween("20160228", "20160301", "yyyyMMdd");
+        System.out.println(d);
+        assertThat(d).isEqualTo(2);
+
+        d = DateTimeUtil.daysBetween("2016-02-28", "2016-03-01", "yyyy-MM-dd");
+        System.out.println(d);
+        assertThat(d).isEqualTo(2);
+
+        d = DateTimeUtil.daysBetween("20160228", "20170228", "yyyyMMdd");
+        System.out.println(d);
+        assertThat(d).isEqualTo(366);
+
+        d = DateTimeUtil.daysBetween("20170807", "20160807", "yyyyMMdd");
+        System.out.println(d);
+        assertThat(d).isEqualTo(365);
+        
+        d = DateTimeUtil.daysBetween(1502035200000L, 1470499200000L);
+        System.out.println(d);
+        assertThat(d).isEqualTo(365);
         
     }
     
